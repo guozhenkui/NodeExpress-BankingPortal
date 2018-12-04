76692cb2bde3d99999fc281ab3e3ed1249ffd301
guozhenkui <41421366+guozhenkui@users.noreply.github.com>
2018-12-04 10:41:15 +0800

    Using the Express Router

--- src/app.js
+++ src/app.js
@@ -15,26 +15,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => { res.render('index', { title: 'Account Summary', accounts});});
-app.get('/savings', (req, res) => { res.render('account', { account: accounts.savings });});
-app.get('/checking', (req, res) => { res.render('account', { account: accounts.checking });});
-app.get('/credit', (req, res) => { res.render('account', { account: accounts.credit });});
+
 app.get('/profile', (req, res) => { res.render('profile', { user: users[0] });});
-app.get('/transfer', (req, res) => res.render('transfer'));
 
-app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-  writeJSON();
-  res.render('transfer', { message: 'Transfer Completed'});
-});
+const accountRoutes = require('./routes/account');
+const servicesRoutes = require('./routes/services');
 
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
+app.use('/account', accountRoutes );
 
-app.post('/payment', (req, res) => {
-  accounts.credit.balance -= req.body.amount;
-  accounts.credit.available += parseInt(req.body.amount, 10);
-  writeJSON();
-  res.render('payment', { message: "Payment Successful", account: accounts.credit });
-});
+app.use('/services', servicesRoutes  );
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
