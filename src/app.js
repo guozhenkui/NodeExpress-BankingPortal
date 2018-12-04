3eef2e5215743ad13b97295d6faec2861839726a
guozhenkui <41421366+guozhenkui@users.noreply.github.com>
2018-12-04 10:26:30 +0800

    Creating a Data Access Library

--- src/app.js
+++ src/app.js
@@ -5,17 +5,8 @@ const express = require('express');
 
 const app = express();
 
-const accountData = fs.readFileSync(
-  path.join(__dirname, 'json', 'accounts.json'), 'utf-8'
-);
+const { accounts, users, writeJSON } = require('./data');
 
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-  path.join(__dirname, 'json', 'users.json'), 'utf-8'
-);
-
-const users = JSON.parse(userData);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -33,8 +24,7 @@ app.get('/transfer', (req, res) => res.render('transfer'));
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-  const accountsJSON  = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname , 'json/accounts.json'), accountsJSON , 'utf-8');
+  writeJSON();
   res.render('transfer', { message: 'Transfer Completed'});
 });
 
@@ -43,8 +33,7 @@ app.get('/payment', (req, res) => res.render('payment', { account: accounts.cred
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
-  const accountsJSON  = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname , 'json/accounts.json'), accountsJSON , 'utf-8');
+  writeJSON();
   res.render('payment', { message: "Payment Successful", account: accounts.credit });
 });
 
